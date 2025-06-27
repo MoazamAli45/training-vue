49ff532b5c2f52c33a7e21b1c8483e406987c503
MoazamAli45 <smoazam987@gmail.com>
2025-06-27 15:53:38 +0500

    Vue Router Added

---
+++ src/components/JobListing.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { MapPin } from 'lucide-vue-next'
+import { computed, ref } from 'vue'
+const showFullDescription = ref(false)
+
+const props = defineProps({
+  job: Object,
+})
+
+const toggleDescription = () => {
+  showFullDescription.value = !showFullDescription.value
+}
+
+const truncatedFullDescription = computed(() => {
+  let description = props.job.description
+
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + '...'
+  }
+
+  return description
+})
+</script>
+
+<template>
+  <div class="bg-white rounded-xl shadow-md relative">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-gray-600 my-2">{{ job.type }}</div>
+        <h3 class="text-xl font-bold">{{ job.title }}</h3>
+      </div>
+
+      <div class="mb-5">
+        <div>
+          {{ truncatedFullDescription }}
+          <button @click="toggleDescription" class="text-green-500 hover:text-green-600">
+            {{ showFullDescription ? 'less' : 'More ' }}
+          </button>
+        </div>
+      </div>
+
+      <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
+
+      <div class="border border-gray-100 mb-5"></div>
+
+      <div class="flex flex-col lg:flex-row justify-between mb-4">
+        <div class="text-orange-700 mb-3 flex items-center gap-1">
+          <MapPin class="text-orange-700" />
+
+          <span>{{ job.location }}</span>
+        </div>
+        <a
+          :href="`/job/${job.id}`"
+          class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+        >
+          Read More
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
